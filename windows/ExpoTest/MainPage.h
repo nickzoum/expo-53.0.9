a345ab9b7c0e0e889419b6f5756967a9f20fdcf5
nickzoum <nzoumis@outlook.com>
2025-05-28 19:56:34 +0300

    chore: initialize react native windows
    npx react-native init-windows

---
+++ windows/ExpoTest/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::ExpoTest::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::ExpoTest::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
